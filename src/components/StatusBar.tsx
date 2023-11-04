58605237f19d9b155722225116b268fbf9ec5851
Vladimir Ventura <vladventura@outlook.es>
2023-11-04 18:11:22 -0400

    Initial commit
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

---
+++ src/components/StatusBar.tsx
@@ -0,0 +1,3 @@
+export const StatusBar = () => {
+    return <div>Status Bar</div>
+};
---
+++ src/models/MainScreenItem.ts
@@ -0,0 +1,7 @@
+export interface MainScreenItem {
+    id?: number,
+    flavorText: string,
+    backgroundImage: string,
+    iconImage?: string,
+    icon?: string,
+}
